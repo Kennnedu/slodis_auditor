97086b0a769709f7497d69876f03d06abff6e5f7
Kennnedu <r.kenda94@gmail.com>
2021-08-19 11:35:42 +0300

    set postgres db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_08_17_121908) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string "barcode"
     t.float "amount"
