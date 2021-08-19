fe35d1d9249da3b1cfbd697913bca3e37abc055a
Kennnedu <r.kenda94@gmail.com>
2021-08-19 11:45:39 +0300

    added ability to export products to csv

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,17 @@
+require 'csv'
+
 class Product < ApplicationRecord
   validates :barcode, :amount, presence: true
+
+  def self.to_csv
+    attributes = %w{barcode amount}
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      all.each do |user|
+        csv << attributes.map{ |attr| user.send(attr) }
+      end
+    end
+  end
 end
