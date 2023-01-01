b5df7648d9460d3122d43c211f9002330cdb35a6
Kennnedu <sir.kenda@yandex.ru>
2023-01-01 19:48:13 +0000

    fix validation logic

--- app/javascript/controllers/barcode_input_validator_controller.js
+++ app/javascript/controllers/barcode_input_validator_controller.js
@@ -3,12 +3,23 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="barcode-input-validator"
 export default class extends Controller {
   connect() {
-    this.element.addEventListener('change', validate);
+    this.element.addEventListener('keyup', this.validate);
   }
 
   async validate(e) {
-    const barcode = e.target.value;
+    const element = e.target
+    const barcode = element.value;
     const result = await fetch(`/product_kinds.json?barcode=${barcode}`)
-    console.log(result);
+    const resJson = await result.json()
+
+    if(resJson.length) {
+      // valid
+      element.classList.add('is-valid');
+      element.classList.remove('is-invalid');
+    } else {
+      // invalid
+      element.classList.add('is-invalid');
+      element.classList.remove('is-valid');
+    }
   }
 }
