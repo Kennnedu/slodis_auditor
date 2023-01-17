5766f80e7c4471245b8cebebf024b15de6183efc
Kennnedu <r.kenda94@gmail.com>
2023-01-17 19:50:06 +0300

    fixed logic & add styles for barcode_storage_checker

--- app/javascript/controllers/barcode_storage_checker_controller.js
+++ app/javascript/controllers/barcode_storage_checker_controller.js
@@ -3,33 +3,64 @@ import debounce from 'src/debounce'
 
 // Connects to data-controller="barcode-storage-checker"
 export default class extends Controller {
+  static classes = [ "loading", "absent", "exist" ]
+  static targets = [ "input" ]
+
   connect() {
-    this.barcode = this.element.value;
-    this.element.addEventListener('keyup', debounce(() => this.call(), 800));
-    this.element.addEventListener('change', debounce(() => this.call(), 800));
+    this.barcode = this.inputTarget.value;
+    this.inputTarget.addEventListener('keyup', debounce(() => this.call(), 800));
+    this.inputTarget.addEventListener('change', debounce(() => this.call(), 800));
   }
 
   async call() {
-    const element = this.element
-    const newBarcode = element.value;
+    const input = this.inputTarget
+    const newBarcode = input.value;
 
+    // clear state when input is empty
+    if(!newBarcode) {
+      this.clearState();
+      return null
+    }
+
+    // do nothing when barcode not changed
     if (this.barcode === newBarcode) {
       return null;
     }
 
+    this.clearState();
     this.barcode = newBarcode;
+    this.toggleLoading();
 
     const result = await fetch(`/product_kinds.json?barcode=${newBarcode}`)
     const resJson = await result.json()
 
+    this.toggleLoading();
+
     if(resJson.length) {
       // valid
-      element.classList.add('is-valid');
-      element.classList.remove('is-invalid');
+      this.element.classList.add(this.existClass);
     } else {
       // invalid
-      element.classList.add('is-invalid');
-      element.classList.remove('is-valid');
+      this.element.classList.add(this.absentClass);
     }
+  }
+
+  toggleLoading() {
+    if(this.loading) {
+      this.loading.remove();
+      this.loading = null;
+    } else {
+      this.loading = document.createElement('div');
+      this.loadingClasses.forEach(el => this.loading.classList.add(el))
+      this.element.append(this.loading);
+    }
+  }
+
+  // clear previous state
+  clearState() {
+    this.element.classList.remove(this.existClass);
+    this.element.classList.remove(this.absentClass);
+    this.inputTarget.classList.remove('valid');
+    this.inputTarget.classList.remove('is-invalid');
   }
 }
